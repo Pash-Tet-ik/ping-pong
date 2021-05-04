3be5968f6f64a9563e63308130ed32fa210a1f88
Pash-Tet-ik <80480749+Pash-Tet-ik@users.noreply.github.com>
2021-05-04 17:44:48 +0300

    Add files via upload

--- cod.js
+++ cod.js
@@ -1,86 +1,112 @@
 var canvas = document.getElementById("myCanvas")
 var ctx = canvas.getContext("2d")
-var tbl = canvas.getContext("2d")
 
-var s=0
-var tx=5
-var ty=5
-var tdy=0
-var shx=16
-var shy=5
-var shdx=3
-var shdy=3
-var colorid = ["#ff0000","#ffa500","#ffd800"," #008000","#0000ff"," #4b0082","#ee82ee"]
-var colorch = 0
+var score = 0 //счёт
+var s=0 //буфер
+var tx=5 //положение пластины x
+var ty=5 //положение пластины y
+var tdy=0 //скорость пластины
+var shx=16 //положение шара x
+var shy=Math.random()*canvas.height //положение шара y
+var shdx=2 //скорость шара x
+var shdy=(Math.random()-0.5)*8 //скорость шара y
+var colorid = ["#ff0000","#d84b20","#ffd800","#96fa00"," #008000","#42aaff","#0000ff"," #4b0082","#ee82ee"] //радуга
+var colorch = 0 //цикл радуги
+var textlosesize = canvas.width/8 + "px Tahoma" //шрифт
 
 function keybordup(){
-    console.log('keyCode: ', event.which);
-    if(event.which==83||event.which==87){
+    if((event.which==83 && tdy==5)||(event.which==87 && tdy==-5)){
         tdy=0
     }
 }
 function keyborddown(){
-    console.log('keyCode: ', event.which);
     if(event.which==87){
         tdy=-5
     }
     else if(event.which==83){
         tdy=5
-    }
+    } 
 }
 
 function draw(){
+//очистка
     ctx.clearRect(0,0, canvas.width, canvas.height)
+//шар
     ctx.beginPath()
-    ctx.arc (shx,shy,5,0,Math.PI*2);
+    ctx.arc (shx,shy,7,0,Math.PI*2);
     ctx.fillStyle = colorid[colorch]
     ctx.fill()
     ctx.closePath()
+//пластина
+    ctx.beginPath()
+    ctx.fillStyle = "black"
+    ctx.fillRect (tx,ty,10,100);
+    ctx.closePath()
 
-    tbl.beginPath()
-    tbl.fillStyle = "black"
-    tbl.fillRect (tx,ty,10,100);
-    tbl.closePath()
+    if (shx<4){
+        ctx.font = textlosesize
+        ctx.fillText("you lose", canvas.width/3.5, canvas.height/2.5);
+        ctx.fillText("score:"+score, canvas.width/3.5, canvas.height/1.6);
+        
+    }
+}
+//смэрц
+function lose(){
+    shx=20
+    shy=Math.random()*canvas.height
+    shdy=(Math.random()-0.5)*7
+    shdx=3
+    score = 0
 }
 
 function run(){
+//движениеw
     shx=shx+shdx
     shy=shy+shdy
     ty=ty+tdy
-
+    if(tx<0){
+        tx=0
+    }
+//контроль выхода пластины за границу
+    if(ty<0){
+        ty=0
+    }
+    if(ty>canvas.height-100){
+        ty=canvas.height-100
+    }
+//отскок справа
     if (shx>canvas.width){
         shdx=-shdx
         colorch=colorch+1
+//отскок сверху и снизу
     }
-    if (shy>canvas.height || shy<0){
+    if (shy+7>canvas.height || shy<0){
         shdy=-shdy
         colorch=colorch+1
     }
-    if (shx<16 && shx>12 && shy>ty && shy<ty+100){
+//отскок от пластины
+    if (shx<16 && shx>16+1.4*shdx && shy>ty-2 && shy<ty+102){
         shdx=-1.1*shdx
-        s=Math.random()-0.5
-        s=parseFloat(s.toFixed(1))
-        shdy=shdy+s
+        shdy=shdy+(tdy/10)
+        shdy=shdy+Math.random()-0.5
+        score = score+1
+        colorch=colorch+1
     }
-    if (shx<0){
-        alert("you lose")
-        shx=16
-        shy=160
-        shdx=3
+//левый край=смэрц
+    if (shx<4 && shdx != 0){
+        shdx=0
+        shdy=0
+        setTimeout(lose,1500)
+//радуга
     }
-    if (colorch == 7){
+    if (colorch >= 9){
             colorch = 0
     }
+
     draw()
-}
+}s
 
-function cl(){
-    console.log( document.getElementById("ddx").value)
-    shdx= parseInt(document.getElementById("ddx").value)
-    shdy= parseInt(document.getElementById("ddy").value)
-}
-
-setInterval(run, 10)
+setInterval(run, 9)
 
 document.addEventListener('keydown',keyborddown)
 document.addEventListener('keyup',keybordup)
